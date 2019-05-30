2265d23b35e6e12e69b08849b05ca708b6f288b3
freebz <freebz@lfpartners.co.kr>
2019-05-30 15:52:11 +0900

    2장 - 첫 번째 자바 모듈 만들기

---
+++ ch02/code2/java9/src/packt.addressbook/packt/addressbook/Main.java
@@ -0,0 +1,15 @@
+package packt.addressbook;
+
+import java.util.*;
+import packt.addressbook.model.*;
+import packt.addressbook.util.*;
+
+public class Main {
+	public static void main(String[] args) {
+		ContactUtil contactUtil = new ContactUtil();
+		SortUtil sortUtil = new SortUtil();
+		List<Contact> contacts = contactUtil.getContacts();
+		sortUtil.sortList(contacts);
+		System.out.println(contacts);
+	}
+}
