855d4a60e9933716950907234ca22903477a0726
freebz <freebz@lfpartners.co.kr>
2019-06-05 16:55:40 +0900

    10장 - 자바 9를 위한 코드 준비하기

---
+++ ch10/02-non-standard-api/src/packt/app/App.java
@@ -0,0 +1,12 @@
+package packt.app;
+
+import sun.misc.BASE64Encoder;
+import sun.util.calendar.CalendarUtils;
+
+public class App {
+
+    public static void main(String[] args) {
+        BASE64Encoder enc = new BASE64Encoder();
+        CalendarUtils.isGregorianLeapYear(2018);
+    }
+}
