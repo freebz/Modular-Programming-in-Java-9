c25eeaf4650442dab2d8bc044a6c6d04fc6e9db8
freebz <freebz@lfpartners.co.kr>
2019-06-05 09:27:32 +0900

    8장 - 링킹 이해하기와 jlink 활용하기

---
+++ ch08/code/java9/src/packt.contact/packt/contact/util/ContactLoadException.java
@@ -0,0 +1,11 @@
+package packt.contact.util;
+
+public class ContactLoadException extends Exception {
+
+    public ContactLoadException() {
+        super();
+    }
+    public ContactLoadException(String message) {
+        super(message);
+    }
+}
