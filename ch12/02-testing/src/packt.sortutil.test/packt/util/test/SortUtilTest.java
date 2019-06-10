158201bf6f4af73f60b60b34677b3157cae1dcdf
freebz <freebz@lfpartners.co.kr>
2019-06-10 14:38:45 +0900

    12장 - 빌드 도구 활용과 자바 모듈 테스트

---
+++ ch12/02-testing/src/packt.sortutil.test/packt/util/test/SortUtilTest.java
@@ -0,0 +1,34 @@
+package packt.util.test;
+
+import java.util.List;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import org.junit.Test;
+import org.junit.Before;
+
+import packt.util.SortUtil;
+
+public class SortUtilTest {
+
+    private SortUtil sortUtil;
+    @Before public void setUp() {
+        sortUtil = new SortUtil();
+    }
+
+    @Test
+    public void testReturnsSameSize() {
+        List out = sortUtil.sortList(Arrays.asList("b", "a", "c"));
+        SortUtil sortutil = new SortUtil();
+        assert out.size() == 3;
+    }
+
+    @Test
+    public void sortList() {
+        List out = sortUtil.sortList(Arrays.asList("b", "a", "c"));
+        assert "a".equals(out.get(0));
+        assert "b".equals(out.get(1));
+        assert "c".equals(out.get(2));
+    }
+
+}
